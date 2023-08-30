0850b1ca89a6e42984b6ef4a104fd1347ec6a79b
gammarkin <marcosrbnsf@gmail.com>
2023-08-29 22:01:34 -0300

    fix: changes mongo localhost url to match node v6+

--- back/src/models/connection.ts
+++ back/src/models/connection.ts
@@ -1,7 +1,7 @@
 import {connect} from 'mongoose';
 import 'dotenv/config';
 
-const MONGO_DB_URL = 'mongodb://localhost:27017';
+const MONGO_DB_URL = 'mongodb://0.0.0.0:27017/';
 
 const connectToDatabase = (
 	mongoDatabaseURI = process.env.MONGO_URI || MONGO_DB_URL
