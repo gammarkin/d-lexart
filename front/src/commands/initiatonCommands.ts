edf78558f8b64ce61ac6f657b04495abff733891
gammarkin <marcosrbnsf@gmail.com>
2023-08-31 00:38:59 -0300

    feat: adds text commands

---
+++ front/src/commands/initiatonCommands.ts
@@ -0,0 +1,12 @@
+export default [
+	'init',
+	'initiation',
+	'initiate',
+	'hello',
+	'hi',
+	'hey',
+	'yo',
+	'sup',
+	'i want',
+	'good',
+];
---
+++ front/src/commands/loanResponses.ts
@@ -0,0 +1,1 @@
+export default ['Do you want to apply for a loan?', 'Loan conditions', 'Help'];
